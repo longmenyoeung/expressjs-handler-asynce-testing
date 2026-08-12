91a95083335024480dd9416fce58f53ed18b2acc
longmenyoeung <longmenyoeung@gmail.com>
2026-08-12 22:27:06 +0700

    testing multer

--- server.js
+++ server.js
@@ -9,6 +9,22 @@ const bookRoute = require("./src/routes/book.route");
 const brandRoute = require("./src/routes/brand.route");
 const categoryRoute = require("./src/routes/category.route");
 const productRoute = require("./src/routes/product.route");
+const multer = require('multer');
+const path = require('path')
+
+
+
+//upload image
+const storage = multer.diskStorage({
+    destination: (req, file, cb) =>  {
+        cb(null, 'uploads/')
+    },
+    filename: (req, file, cb) => {
+        const unique = Date.now() + '_' + Math.round(Math.random() * 1E9)
+        cb(null, file.fieldname + '_' + unique + path.extname(file.originalname));
+    }
+}) 
+const uploads = multer({storage: storage})
 
 
 //PORT 
@@ -29,6 +45,17 @@ app.use('/api/books', bookRoute);
 app.use('/api/brands', brandRoute);
 app.use('/api/categories', categoryRoute);
 app.use('/api/products', productRoute);
+
+//tesing upload image
+app.post('/upload', uploads.single('file'), (req,res) => {
+    console.log(req.file)
+    console.log(req.body)
+
+    res.status(201).json({
+        message: 'Uploaded file successfully.',
+        file:req.file
+    })
+})
 
 
 //5. Error-Handling Middleware ===
